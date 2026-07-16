217c86114af28cb9c80edb88e07b85308c817a70
AleinxDot <agonzalesp@unasam.edu.pe>
2026-07-16 00:10:56 -0500

    feat: enhance IpAddrKind enum with tuple variants and add Point struct example

--- src/constructs.rs
+++ src/constructs.rs
@@ -1,9 +1,30 @@
+//Example of enums in Rust
 pub fn enums(){
-    let ipusuario :IpAddrKind = IpAddrKind::V4;
-    println!("{:?}", ipusuario);
-}
-#[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
+    enum IpAddrKind {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+    //Struct with enum
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+
+    // Example of struct Point
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {x}"),
+    }
+
 }
